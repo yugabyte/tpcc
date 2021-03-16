7f2f2b217516122990e7749546cb167322577f99
robertsami <robertsami@users.noreply.github.com>
2021-03-16 11:20:06 -0400

    Pull command line and config file option readers into separate components (#90)

---
+++ src/com/oltpbenchmark/CommandLineOptions.java
@@ -0,0 +1,175 @@
+package com.oltpbenchmark;
+
+import org.apache.commons.cli.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class CommandLineOptions {
+    private CommandLine argsLine;
+
+    private static Options COMMAND_LINE_OPTS = new Options();
+    static {
+        COMMAND_LINE_OPTS.addOption("h", "help", false, "Print this help");
+        COMMAND_LINE_OPTS.addOption(
+                null,
+                "create",
+                true,
+                "Initialize the database for this benchmark");
+        COMMAND_LINE_OPTS.addOption(
+                null,
+                "clear",
+                true,
+                "Clear all records in the database for this benchmark");
+        COMMAND_LINE_OPTS.addOption(
+                null,
+                "load",
+                true,
+                "Load data using the benchmark's data loader");
+        COMMAND_LINE_OPTS.addOption(
+                null,
+                "execute",
+                true,
+                "Execute the benchmark workload");
+
+
+        COMMAND_LINE_OPTS.addOption(
+                "c",
+                "config",
+                true,
+                "Workload configuration file [default: config/workload_all.xml]");
+
+        COMMAND_LINE_OPTS.addOption("im", "interval-monitor", true,
+                "Throughput Monitoring Interval in milliseconds");
+        COMMAND_LINE_OPTS.addOption(null, "histograms", false, "Print txn histograms");
+        COMMAND_LINE_OPTS.addOption(null, "output-raw", true, "Output raw data");
+        COMMAND_LINE_OPTS.addOption(null, "output-samples", true, "Output sample data");
+        COMMAND_LINE_OPTS.addOption(null, "nodes", true, "comma separated list of nodes (default 127.0.0.1)");
+        COMMAND_LINE_OPTS.addOption(null, "warehouses", true, "Number of warehouses (default 10)");
+        COMMAND_LINE_OPTS.addOption(null, "start-warehouse-id", true, "Start warehouse id");
+        COMMAND_LINE_OPTS.addOption(null, "total-warehouses", true,
+                "Total number of warehouses across all executions");
+        COMMAND_LINE_OPTS.addOption(null, "loaderthreads", true, "Number of loader threads (default 10)");
+        COMMAND_LINE_OPTS.addOption(null, "enable-foreign-keys", true, "Whether to enable foregin keys");
+        COMMAND_LINE_OPTS.addOption(null, "create-sql-procedures", true, "Creates the SQL procedures");
+        COMMAND_LINE_OPTS.addOption(null, "warmup-time-secs", true, "Warmup time in seconds for the benchmark");
+        COMMAND_LINE_OPTS.addOption(null, "initial-delay-secs", true,
+                "Delay in seconds for starting the benchmark");
+        COMMAND_LINE_OPTS.addOption(null, "num-connections", true, "Number of connections used");
+        COMMAND_LINE_OPTS.addOption(null, "merge-results", true, "Merge results from various output files");
+        COMMAND_LINE_OPTS.addOption(null, "dir", true, "Directory containing the csv files");
+    }
+
+    public CommandLineOptions() {}
+
+    public enum Mode {
+        HELP,
+        CREATE,
+        CLEAR,
+        LOAD,
+        EXECUTE,
+        MERGE_RESULTS
+    }
+
+    void init(String[] args) throws ParseException {
+        CommandLineParser parser = new PosixParser();
+        argsLine = parser.parse(COMMAND_LINE_OPTS, args);
+    }
+
+    private boolean isBooleanOptionSet(String key) {
+        if (argsLine.hasOption(key)) {
+            String val = argsLine.getOptionValue(key);
+            return val != null && val.equalsIgnoreCase("true");
+        }
+        return false;
+    }
+
+    public Mode getMode() {
+        if (argsLine.hasOption("h")) return Mode.HELP;
+        if (isBooleanOptionSet("clear")) return Mode.CLEAR;
+        if (isBooleanOptionSet("create")) return Mode.CREATE;
+        if (isBooleanOptionSet("load")) return Mode.LOAD;
+        if (isBooleanOptionSet("execute")) return Mode.EXECUTE;
+        assert isBooleanOptionSet("merge-results");
+        return Mode.MERGE_RESULTS;
+    }
+
+    public void printHelp() {
+        HelpFormatter hlpfrmt = new HelpFormatter();
+        hlpfrmt.printHelp("tpccbenchmark", COMMAND_LINE_OPTS);
+    }
+
+    public Optional<String> getStringOpt(String key) {
+        if (argsLine.hasOption(key)) {
+            return Optional.of(argsLine.getOptionValue(key));
+        }
+        return Optional.empty();
+    }
+
+    public Optional<String> getConfigFile() {
+        return getStringOpt("c");
+    }
+
+    private Optional<Integer> getIntOpt(String key) {
+        if (argsLine.hasOption(key)) {
+            return Optional.of(Integer.parseInt(argsLine.getOptionValue(key)));
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Integer> getIntervalMonitor() {
+        return getIntOpt("im");
+    }
+
+    public Optional<List<String>> getNodes() {
+        if (argsLine.hasOption("nodes")) {
+            return Optional.of(Arrays.asList(argsLine.getOptionValue("nodes").split(",")));
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Integer> getWarehouses() {
+        return getIntOpt("warehouses");
+    }
+
+    public Optional<Integer> getStartWarehouseId() {
+        return getIntOpt("start-warehouse-id");
+    }
+
+    public Optional<Integer> getTotalWarehouses() {
+        return getIntOpt("total-warehouses");
+    }
+
+    public Optional<Integer> getLoaderThreads() {
+        return getIntOpt("loaderthreads");
+    }
+
+    public Optional<Integer> getNumDbConnections() {
+        return getIntOpt("num-connections");
+    }
+
+    public Optional<Integer> getWarmupTime() {
+        return getIntOpt("num-connections");
+    }
+
+    public Optional<Integer> getInitialDelaySeconds() {
+        return getIntOpt("initial-delay-secs");
+    }
+
+    public boolean getIsEnableForeignKeysSet() {
+        return isBooleanOptionSet("enable-foreign-keys");
+    }
+
+    public boolean getIsCreateSqlProceduresSet() {
+        return isBooleanOptionSet("create-sql-procedures");
+    }
+
+    public Optional<String> getDirPath() {
+        return getStringOpt("dir");
+    }
+
+    public boolean getIsOutputMetricHistogramsSet() {
+        return isBooleanOptionSet("histograms");
+    }
+}
---
+++ src/com/oltpbenchmark/ConfigFileOptions.java
@@ -0,0 +1,135 @@
+package com.oltpbenchmark;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.XMLConfiguration;
+import org.apache.commons.configuration.tree.xpath.XPathExpressionEngine;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class ConfigFileOptions {
+    XMLConfiguration xmlConfig;
+
+    ConfigFileOptions(String filePath) throws ConfigurationException {
+        xmlConfig = new XMLConfiguration(filePath);
+        xmlConfig.setExpressionEngine(new XPathExpressionEngine());
+    }
+
+    public String getDbDriver() {
+        return xmlConfig.getString("driver");
+    }
+
+    public String getDbName() {
+        return xmlConfig.getString("DBName");
+    }
+
+    public String getDbUsername() {
+        return xmlConfig.getString("username");
+    }
+
+    public String getDbPassword() {
+        return xmlConfig.getString("password");
+    }
+
+    private Optional<String> getStringOpt(String key) {
+        if (xmlConfig.containsKey(key) && xmlConfig.getString(key).length() > 0) {
+            return Optional.of(xmlConfig.getString(key));
+        }
+        return Optional.empty();
+    }
+
+    public Optional<String> getSslCert() {
+        return getStringOpt("sslCert");
+    }
+
+    public Optional<String> getSslKey() {
+        return getStringOpt("sslKey");
+    }
+
+    public Optional<String> getJdbcUrl() {
+        return getStringOpt("jdbcURL");
+    }
+
+    public Optional<String> getIsolationLevel() {
+        return getStringOpt("isolation");
+    }
+
+    private Optional<Boolean> getBoolOpt(String key) {
+        if (xmlConfig.containsKey(key)) {
+            return Optional.of(xmlConfig.getBoolean(key));
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Boolean> getUseKeyingTime() {
+        return getBoolOpt("useKeyingTime");
+    }
+
+    public Optional<Boolean> getUseThinkTime() {
+        return getBoolOpt("useThinkTime");
+    }
+
+    public Optional<Boolean> getEnableForeignKeysAfterLoad() {
+        return getBoolOpt("enableForeignKeysAfterLoad");
+    }
+
+    public Optional<Boolean> getTrackPerSQLStmtLatencies() {
+        return getBoolOpt("trackPerSQLStmtLatencies");
+    }
+
+    public Optional<Boolean> getUseStoredProcedures() {
+        return getBoolOpt("useStoredProcedures");
+    }
+
+    private Optional<Integer> getIntOpt(String key) {
+        if (xmlConfig.containsKey(key)) {
+            return Optional.of(xmlConfig.getInt(key));
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Integer> getBatchSize() {
+        return getIntOpt("batchSize");
+    }
+
+    public Optional<Integer> getMaxRetriesPerTransaction() {
+        return getIntOpt("maxRetriesPerTransaction");
+    }
+
+    public Optional<Integer> getMaxLoaderRetries() {
+        return getIntOpt("maxLoaderRetries");
+    }
+
+    public Optional<Integer> getPort() {
+        return getIntOpt("port");
+    }
+
+    public Optional<Integer> getHikariConnectionTimeoutMs() {
+        return getIntOpt("hikariConnectionTimeoutMs");
+    }
+
+    public int getTransactionTypeCount() {
+        return xmlConfig.getList("transactiontypes/transaction/name").size();
+    }
+
+    public Optional<String> getTransactionTypeName(int idx) {
+        return getStringOpt(String.format("transactiontypes/transaction[%d]/name", idx));
+    }
+
+    public Optional<String> getTransactionTypeWeight(int idx) {
+        return getStringOpt(String.format("transactiontypes/transaction[%d]/weight", idx));
+    }
+
+    public Optional<Integer> getRate() {
+        return getIntOpt("rate");
+    }
+
+    public Optional<Integer> getRuntime() {
+        return getIntOpt("runtime");
+    }
+
+    public Optional<String> getWeightsString() {
+        return getStringOpt("weights");
+    }
+}

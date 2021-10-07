4629dd7aab770e9fbc2bf22965ae927849e31bdd
Hemant Bhanawat <hbhanawat@yugabyte.com>
2021-10-07 11:12:59 +0530

    fix

--- src/com/oltpbenchmark/api/Worker.java
+++ src/com/oltpbenchmark/api/Worker.java
@@ -481,8 +481,8 @@ public class Worker implements Runnable {
             startConnection = System.nanoTime();
 
             conn = dataSource.getConnection();
-            conn.createStatement().executeQuery("set yb_debug_log_internal_restarts=true").next();
-            conn.createStatement().executeQuery("set yb_debug_log_docdb_requests=true").next();
+            conn.createStatement().execute("set yb_debug_log_internal_restarts=true");
+            conn.createStatement().execute("set yb_debug_log_docdb_requests=true");
             if (next.getProcedureClass() != StockLevel.class) {
                 // In accordance with 2.8.2.3 of the TPCC spec, StockLevel should execute each query in its own Snapshot
                 // Isolation.

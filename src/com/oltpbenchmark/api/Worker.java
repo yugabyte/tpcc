0760abcb319294382a1413e9d3345d54e57c3834
Sonal Agarwal <sagarwal@ip-192-168-1-5.us-west-2.compute.internal>
2022-07-07 15:21:02 +0530

    - reverting the flag for filter pushdown

--- src/com/oltpbenchmark/api/Worker.java
+++ src/com/oltpbenchmark/api/Worker.java
@@ -481,7 +481,7 @@ public class Worker implements Runnable {
             startConnection = System.nanoTime();
 
             conn = dataSource.getConnection();
-            conn.createStatement().execute("SET yb_enable_expression_pushdown to on");
+            // conn.createStatement().execute("SET yb_enable_expression_pushdown to on");
             if (next.getProcedureClass() != StockLevel.class) {
                 // In accordance with 2.8.2.3 of the TPCC spec, StockLevel should execute each query in its own Snapshot
                 // Isolation.

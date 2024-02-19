299b0651ff3227fef28968631bc1325da96f0fbb
Aleti Vasanth Reddy <vassu@va-mbp-4phvg.tail5955.ts.net>
2024-02-19 11:55:24 +0530

    fixed the default argument of virtual threads

--- src/com/oltpbenchmark/CommandLineOptions.java
+++ src/com/oltpbenchmark/CommandLineOptions.java
@@ -67,7 +67,7 @@ public class CommandLineOptions {
         COMMAND_LINE_OPTS.addOption(null, "merge-json-results", true, "Merge results from various json output files");
         COMMAND_LINE_OPTS.addOption(null, "dir", true, "Directory containing the csv files");
         COMMAND_LINE_OPTS.addOption(null, "vv", false, "Output verbose execute results");
-        COMMAND_LINE_OPTS.addOption("vt", "virtual-threads", true, "Use Virtual threads");
+        COMMAND_LINE_OPTS.addOption("vt", "virtual-threads", false, "Use Virtual threads");
     }
 
     public CommandLineOptions() {}

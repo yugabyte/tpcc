88ce709f14a559af44dbea4e082bc471eb81bc6f
Sonal Agarwal <sagarwal@sa-mbp-x740n.tail5955.ts.net>
2024-12-20 15:36:04 +0530

    - reverting previous commit

--- src/com/oltpbenchmark/ThreadBench.java
+++ src/com/oltpbenchmark/ThreadBench.java
@@ -411,7 +411,6 @@ public class ThreadBench implements Thread.UncaughtExceptionHandler {
                 // Last phase
                 lastEntry = true;
                 testState.startCoolDown();
-                LOG.info("Threads cooled down...");
                 measureEnd = now;
                 LOG.info(StringUtil.bold("TERMINATE") + " :: Waiting for all terminals to finish ..");
               } else if (phase != null) {
@@ -469,7 +468,6 @@ public class ThreadBench implements Thread.UncaughtExceptionHandler {
         // All threads have noticed the done, meaning all measured
         // requests have definitely finished.
         // Time to quit.
-          LOG.info("Thread state found EXIT...");
         break;
       }
     } // WHILE (main loop)
--- src/com/oltpbenchmark/api/Worker.java
+++ src/com/oltpbenchmark/api/Worker.java
@@ -82,12 +82,7 @@ public class Worker implements Runnable {
         try {
             if(!wrkld.getUseHikariPool()) {
                 this.dataSource = null;
-                if(wrkld.getUseCreateConnForEveryTx())
-                    ll_conn =  null;
-                else {
-                    ll_conn = benchmarkModule.makeConnection();
-                    ll_conn.createStatement().execute("SET yb_enable_expression_pushdown to on");
-                    }
+                ll_conn = wrkld.getUseCreateConnForEveryTx() ? null : benchmarkModule.makeConnection();
             } else {   //use Hikari Pool
                 ll_conn = null;
                 this.dataSource = this.benchmarkModule.getDataSource();
@@ -503,10 +498,8 @@ public class Worker implements Runnable {
             } else  //use Hikari connection Pool
                 conn = dataSource.getConnection();
             try {
-                if(wrkld.getDBType().equals("yugabyte")) {
-                    if(ll_conn == null)
-                        conn.createStatement().execute("SET yb_enable_expression_pushdown to on");
-                }
+                if(wrkld.getDBType().equals("yugabyte"))
+                    conn.createStatement().execute("SET yb_enable_expression_pushdown to on");
                 if (next.getProcedureClass() != StockLevel.class) {
                     // In accordance with 2.8.2.3 of the TPCC spec, StockLevel should execute each query in its own Snapshot
                     // Isolation.
